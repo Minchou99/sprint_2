7d1c24db5b6aed0e80b65fc4ae5b6a414410e595
Minchou99 <minhchaupt2903@gmail.com>
2023-01-05 21:57:02 +0700

    ChauPTM push task day 4

---
+++ sprint_2_back_end/src/main/java/sprint_2_back_end/Sprint2BackEndApplication.java
@@ -0,0 +1,13 @@
+package sprint_2_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint2BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sprint2BackEndApplication.class, args);
+    }
+
+}
