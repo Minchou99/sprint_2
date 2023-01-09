fde50afabc5acee43b531113163b2a1f70033f23
Minchou99 <minhchaupt2903@gmail.com>
2023-01-09 19:37:23 +0700

    update

---
+++ demo/src/app/app-employee/app-employee.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Employee, HttpClientService} from "../service/httpclient.service";
+
+@Component({
+  selector: 'app-app-employee',
+  templateUrl: './app-employee.component.html',
+  styleUrls: ['./app-employee.component.css']
+})
+export class AppEmployeeComponent implements OnInit {
+  user: Employee = new Employee("","","","");
+
+  constructor(
+    private httpClientService: HttpClientService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  createEmployee(): void {
+    this.httpClientService.createEmployee(this.user)
+      .subscribe( data => {
+        alert("Employee created successfully.");
+      });
+
+  };
+
+
+}
---
+++ demo/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppEmployeeComponent } from './app-employee/app-employee.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    AppEmployeeComponent,
+    FooterComponent,
+    LoginComponent,
+    LogoutComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
